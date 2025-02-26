bd5e0213283d8f3e7ef631d2eb54310e68b0c353
Yuchan Lee <lasuillard@gmail.com>
2025-02-26 10:38:05 +0000

    feat: ECS task definition pruning

--- aws_annoying/ecs_task_definition_lifecycle.py
+++ aws_annoying/ecs_task_definition_lifecycle.py
@@ -1,11 +1,32 @@
 from __future__ import annotations
 
+import boto3
+import typer
 from rich import print  # noqa: A004
 
 from .app import app
 
 
 @app.command()
-def ecs_task_definition_lifecycle() -> None:
+def ecs_task_definition_lifecycle(
+    *,
+    family: str = typer.Option(
+        ...,
+        help="The name of the task definition family.",
+        show_default=False,
+    ),
+    keep_latest: int = typer.Option(
+        ...,
+        help="Number of latest (revision) task definitions to keep.",
+        show_default=False,
+        min=1,
+        max=100,
+    ),
+) -> None:
     """Execute ECS task definition lifecycle."""
-    print("Handling ECS task definition lifecycle...")
+    ecs = boto3.client("ecs")
+    task_definitions = ecs.list_task_definitions(familyPrefix=family, status="ACTIVE")
+    expired_defs = task_definitions["taskDefinitionArns"][:-keep_latest]
+    for arn in expired_defs:
+        ecs.deregister_task_definition(taskDefinition=arn)
+        print(f"✅ Deregistered task definition [yellow]{arn!r}[/yellow]")
--- tests/test_ecs_task_definition_lifecycle.py
+++ tests/test_ecs_task_definition_lifecycle.py
@@ -1,2 +1,63 @@
+import boto3
+from typer.testing import CliRunner
+
+from aws_annoying.main import app
+
+runner = CliRunner()
+
+
 def test_nothing() -> None:
-    assert 1 + 1 == 2
+    """If nothing is provided, the command should do nothing."""
+    # Arrange
+    # ...
+
+    # Act
+    result = runner.invoke(
+        app,
+        [
+            "load-variables",
+        ],
+    )
+
+    # Assert
+    assert result.exit_code == 0
+    assert result.stdout == ""
+
+
+def test_ecs_task_definition_lifecycle() -> None:
+    """If nothing is provided, the command should do nothing."""
+    # Arrange
+    ecs = boto3.client("ecs")
+    family = "my-task"
+    for i in range(25):
+        ecs.register_task_definition(
+            family=family,
+            containerDefinitions=[
+                {
+                    "name": "my-container",
+                    "image": f"my-image:{i}",
+                    "cpu": 0,
+                    "memory": 0,
+                },
+            ],
+        )
+
+    # Act
+    result = runner.invoke(
+        app,
+        [
+            "ecs-task-definition-lifecycle",
+            "--family",
+            family,
+            "--keep-latest",
+            "10",
+        ],
+    )
+
+    # Assert
+    assert result.exit_code == 0
+    assert result.stdout != ""
+
+    # TODO(lasuillard): Moto does not support filtering by status yet
+    # assert len(ecs.list_task_definitions(familyPrefix=family, status="INACTIVE")["taskDefinitionArns"]) == 15  # noqa: ERA001, E501
+    # assert len(ecs.list_task_definitions(familyPrefix=family, status="ACTIVE")["taskDefinitionArns"]) == 10  # noqa: ERA001, E501
