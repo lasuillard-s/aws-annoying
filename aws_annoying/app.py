fb6baabc40b3dab6c0b6d6cb5061dd11b5c75751
Yuchan Lee <lasuillard@gmail.com>
2025-03-21 22:23:13 +0900

    Configure AWS MFA (#16)

--- aws_annoying/app.py
+++ aws_annoying/app.py
@@ -6,4 +6,5 @@ app = typer.Typer(
     pretty_exceptions_short=True,
     pretty_exceptions_show_locals=False,
     rich_markup_mode="rich",
+    no_args_is_help=True,
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import subprocess
+from configparser import ConfigParser
 from typing import TYPE_CHECKING, Protocol
 
 import pytest
@@ -12,18 +13,40 @@ from typer.testing import CliRunner
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
+    from pathlib import Path
 
 runner = CliRunner()
 
 
+@pytest.fixture
+def set_terminal_width() -> int:
+    """Set console width."""
+    return 200
+
+
 @pytest.fixture(autouse=True)
-def aws_credentials(monkeypatch: pytest.MonkeyPatch) -> None:
-    """Mock AWS Credentials for Moto."""
-    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "testing")
-    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "testing")
-    monkeypatch.setenv("AWS_SECURITY_TOKEN", "testing")
-    monkeypatch.setenv("AWS_SESSION_TOKEN", "testing")
-    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-east-1")
+def patch_terminal_width(monkeypatch: pytest.MonkeyPatch, set_terminal_width: int) -> None:
+    """Patch the console width."""
+    monkeypatch.setenv("COLUMNS", str(set_terminal_width))
+
+
+@pytest.fixture(autouse=True)
+def aws_credentials(monkeypatch: pytest.MonkeyPatch, tmp_path: Path) -> None:
+    """Mock AWS Credentials."""
+    aws_credentials = tmp_path / "credentials"
+    ini = ConfigParser()
+    ini["default"] = {
+        "aws_access_key_id": "testing",
+        "aws_secret_access_key": "testing",
+        "aws_session_token": "testing",
+        "aws_security_token": "testing",
+        "region": "us-east-1",
+    }
+    with aws_credentials.open("w") as f:
+        ini.write(f)
+
+    monkeypatch.setenv("AWS_SHARED_CREDENTIALS_FILE", str(aws_credentials))
+    monkeypatch.setenv("AWS_CONFIG_FILE", str(tmp_path / "config"))
 
     # Intentionally malform `AWS_ENDPOINT_URL` environment variable to prevent running tests from the user environment
     # which highly likely malicious to the user's AWS account.
