b1c32393418e7d59f93f4045acedb56e12697e28
Yuchan Lee <lasuillard@gmail.com>
2025-04-07 13:22:18 +0000

    chore: Add comment

--- aws_annoying/session_manager/start.py
+++ aws_annoying/session_manager/start.py
@@ -6,4 +6,4 @@ from ._app import session_manager_app
 @session_manager_app.command()
 def start() -> None:
     """Start new session."""
-    # TODO(lasuillard): To be implemented
+    # TODO(lasuillard): To be implemented (maybe in #24?)
