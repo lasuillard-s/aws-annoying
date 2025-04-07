e591808ec8c2198b1f157d56dfc0937fc6419e46
Yuchan Lee <lasuillard@gmail.com>
2025-04-07 13:37:57 +0000

    ci: Add workflow matrix for Windows and macOS

---
+++ tests/session_manager/test_session_manager.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.macos
+def test_macos_session_manager_install() -> None:
+    pytest.fail("Not implemented yet.")
+
+
+@pytest.mark.windows
+def test_windows_session_manager_install() -> None:
+    pytest.fail("Not implemented yet.")
