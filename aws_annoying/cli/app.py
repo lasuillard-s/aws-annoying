12d883401fa16b9e7a575e30c69c923cc88a2b49
Yuchan Lee <lasuillard@gmail.com>
2025-05-23 13:31:41 +0000

    feat: Add global flags `--quiet` and `--verbose` (`-v`, `-vv`, `-vvv`)

--- aws_annoying/cli/app.py
+++ aws_annoying/cli/app.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import logging
+
 import typer
+from rich.logging import RichHandler
 
 app = typer.Typer(
     pretty_exceptions_short=True,
@@ -8,3 +11,35 @@ app = typer.Typer(
     rich_markup_mode="rich",
     no_args_is_help=True,
 )
+
+
+@app.callback()
+def main(  # noqa: D103
+    *,
+    quiet: bool = typer.Option(
+        False,  # noqa: FBT003
+        "--quiet",
+        help="Disable outputs.",
+    ),
+    verbosity: int = typer.Option(
+        0,
+        "--verbose",  # `--verbose --verbose --verbose` equals to `-vvv`, but normally won't be used
+        "-v",
+        count=True,
+        help="Increase verbosity level. Use `-vvv` for full output.",
+        min=0,
+        max=3,
+        show_default=False,
+    ),
+) -> None:
+    if not quiet:
+        log_level = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG, 3: logging.NOTSET}.get(verbosity)
+        logging.basicConfig(
+            level=log_level,
+            format="%(message)s",
+            datefmt="[%X]",
+            handlers=[
+                # Reproduce `rich.print` behavior
+                RichHandler(show_time=False, show_level=False, markup=True),
+            ],
+        )
