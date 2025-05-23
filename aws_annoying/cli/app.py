51fde4fcb194a4a427c0e8b932538b1271bd6ebb
Yuchan Lee <lasuillard@gmail.com>
2025-05-23 14:20:48 +0000

    feat!: Simplify verbosity

--- aws_annoying/cli/app.py
+++ aws_annoying/cli/app.py
@@ -20,18 +20,12 @@ def main(  # noqa: D103
         False,  # noqa: FBT003
         help="Disable outputs.",
     ),
-    verbosity: int = typer.Option(
-        0,
-        "--verbose",  # `--verbose --verbose --verbose` equals to `-vvv`, but normally won't be used
-        "-v",
-        count=True,
-        help="Increase verbosity level. Use `-vvv` for full output.",
-        min=0,
-        max=3,
-        show_default=False,
+    verbose: bool = typer.Option(
+        False,  # noqa: FBT003
+        help="Enable verbose outputs.",
     ),
 ) -> None:
-    log_level = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG, 3: logging.NOTSET}[verbosity]
+    log_level = logging.DEBUG if verbose else logging.INFO
     logging_config: logging.config._DictConfigArgs = {
         "version": 1,
         "disable_existing_loggers": False,
