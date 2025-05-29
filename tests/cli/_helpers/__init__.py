af8bf1a9243ecfe7687edce73b527259b17b2858
Yuchan Lee <lasuillard@gmail.com>
2025-05-29 13:12:52 +0000

    test: Refactor big test setup using helpers

--- tests/cli/_helpers/__init__.py
+++ tests/cli/_helpers/__init__.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
+from .boto3 import create_parameters, create_secrets
 from .command_builder import repeat_options
 from .invoke import invoke_cli
 from .string_ import normalize_console_output
 
-__all__ = ("PRINTENV_PY", "invoke_cli", "normalize_console_output", "printenv_py", "repeat_options")
+__all__ = (
+    "PRINTENV_PY",
+    "create_parameters",
+    "create_secrets",
+    "invoke_cli",
+    "normalize_console_output",
+    "printenv_py",
+    "repeat_options",
+)
 
 PRINTENV_PY = (Path(__file__).parent / "scripts" / "printenv.py").absolute()
