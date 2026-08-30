db28791acbe01e4f56dd4d2472f7e51565f61544
Yuchan Lee <lasuillard@gmail.com>
2026-08-30 07:24:03 +0000

    Revert "Fix lint errors"
    This reverts commit a6d4eb70f2342ec4bbb8f8e5c0e8ab713bf5a4e1.

--- aws_annoying/_cli/main.py
+++ aws_annoying/_cli/main.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 try:
     import typer  # noqa: F401
 except ImportError:
-    app = None
+    app = None  # type: ignore[assignment]
 else:
     from . import ecs as _ecs  # noqa: F401
     from . import load_variables as _load_variables  # noqa: F401
